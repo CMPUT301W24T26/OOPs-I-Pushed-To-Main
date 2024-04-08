f2070acdc321b7971107d3d75029265e25f30fa5
AidanGironella <aidangironella@gmail.com>
2024-04-07 22:58:52 -0600

    Fixed checkin list

--- src/OOPsIPushedToMain/app/src/main/java/com/oopsipushedtomain/CheckInList/CheckInListActivity.java
+++ src/OOPsIPushedToMain/app/src/main/java/com/oopsipushedtomain/CheckInList/CheckInListActivity.java
@@ -17,13 +17,16 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.oopsipushedtomain.Database.FirebaseAccess;
+import com.oopsipushedtomain.User;
 import com.oopsipushedtomain.Database.FirebaseInnerCollection;
 import com.oopsipushedtomain.Database.FirestoreAccessType;
+import com.oopsipushedtomain.EventListActivity;
 import com.oopsipushedtomain.R;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
 
 public class CheckInListActivity extends AppCompatActivity {
@@ -47,6 +50,8 @@ public class CheckInListActivity extends AppCompatActivity {
      * The UID of the event to check for
      */
     private String eventId;
+
+    private String userId;
     private final String TAG = "CheckInList";
 
     @Override
@@ -72,7 +77,7 @@ public class CheckInListActivity extends AppCompatActivity {
                     runOnUiThread(() -> {
                         attendeeNameView.setText((String) attendee.get("name"));
                         attendeeImageView.setImageBitmap((Bitmap) attendee.get("image"));
-                        checkInCountView.setText((String) attendee.get("count"));
+                        checkInCountView.setText(attendee.get("count").toString());
                     });
                 }
 
@@ -101,40 +106,82 @@ public class CheckInListActivity extends AppCompatActivity {
      *
      */
     public void getCheckedInAttendees() {
+        // Clear the list
         attendeeDataList.clear();
 
-        CompletableFuture<Void> future = new CompletableFuture<>();
+        // Create a callable
+        Callable<ArrayList<Void>> getDataTask = () -> {
+            // Get the list of checked in events
+            ArrayList<Map<String, Object>> users =  db.getAllDocuments().get();
 
-        db.getAllDocuments().thenAccept(users -> {
-            if (users == null) {
-                future.complete(null);
-                return;
+            // Check if this is null
+            if (users == null){
+                return null;
             }
 
-            for (Map<String, Object> user : users) {
-//                user.forEach((key, value) -> System.out.println(key + " asdfhjkl:!!! " + value));
-                Log.d(TAG, "asdfhjkl " + (String) user.get("UID"));
-                Log.d(TAG, "asdfhjkl " + eventId);
-                db.getDataFromFirestore((String) user.get("UID"), FirebaseInnerCollection.checkedInEvents, eventId).thenAccept(checkIn -> {
-//                    checkIn.forEach((key, value) -> System.out.println(key + " asdfhjkl:??? " + value));
-                    if (checkIn != null) {
-                        Log.d(TAG, "asdfhjkl");
-                        // Create a new attendee from the user's information
+            // Iterate through the users
+            String userId;
+            for (Map<String, Object> user : users){
+                // Create a new event
+                userId = (String) user.get("UID");
+
+                // Get the event details from the database
+                Map<String, Object> checkInData = db.getDataFromFirestore(userId, FirebaseInnerCollection.checkedInEvents, eventId).get();
+                if (checkInData != null) {
+                    // Set the params
+                    if (checkInData.get("UID").equals(eventId)) {
                         Map<String, Object> newAttendee = new HashMap<>();
                         newAttendee.put("name", user.get("name"));
                         newAttendee.put("image", user.get("profileImage"));
-                        newAttendee.put("count", checkIn.get("count"));
+                        newAttendee.put("count", checkInData.get("count"));
 
                         attendeeDataList.add(newAttendee);
                     }
-
-                    if (user.equals(users.get(users.size() - 1))) {
-                        future.complete(null);
-                    }
-                });
+                }
             }
 
-            future.thenAccept(value -> runOnUiThread(() -> attendeeAdapter.notifyDataSetChanged()));
-        });
+            // All done, notify that dataset has changed
+            runOnUiThread(() -> {
+                attendeeAdapter.notifyDataSetChanged();
+            });
+
+            return null;
+
+        };
+
+        // Run the task
+        db.callableToCompletableFuture(getDataTask);
+
+        // TODO OLD
+//        CompletableFuture<Void> future = new CompletableFuture<>();
+//
+//        db.getAllDocuments().thenAccept(users -> {
+//            if (users == null) {
+//                future.complete(null);
+//                return;
+//            }
+//            for (Map<String, Object> user : users) {
+////                Log.d(TAG, "asdfhjkl " + user.get("UID"));
+////                Log.d(TAG, "asdfhjkl " + eventId);
+//                userId = (String) user.get("UID");
+//                db.getDataFromFirestore(userId, FirebaseInnerCollection.checkedInEvents, eventId).thenAccept(checkIn -> {
+////                    checkIn.forEach((key, value) -> System.out.println(key + " asdfhjkl:??? " + value));
+//                    if (checkIn != null) {
+//                        Log.d(TAG, "asdfhjkl");
+//                        // Create a new attendee from the user's information
+//                        Map<String, Object> newAttendee = new HashMap<>();
+//                        newAttendee.put("name", user.get("name"));
+//                        newAttendee.put("image", user.get("profileImage"));
+//                        newAttendee.put("count", checkIn.get("count"));
+//
+//                        attendeeDataList.add(newAttendee);
+//                    }
+//                    if (user.equals(users.get(users.size() - 1))) {
+//                        future.complete(null);
+//                    }
+//                });
+//            }
+//            future.thenAccept(value -> runOnUiThread(() -> attendeeAdapter.notifyDataSetChanged()));
+//        });
     }
 }
